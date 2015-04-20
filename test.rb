e46bbbe40c821b4ddc094cfd40001b815bbcbadc
Peter Vajda <petvajda@gmail.com>
2015-04-20 15:05:12 +0200

    Fix path

--- test.rb
+++ test.rb
@@ -4,7 +4,7 @@ require_relative "kata04"
 class TestKata04 < Test::Unit::TestCase
  
   def test_weather
-  	d = WeatherData.read_file("/Users/pvajda/Desktop/HW/weather.dat")
+  	d = WeatherData.read_file("weather.dat")
     assert_equal([14, 2], WeatherData.analyse())
   end
  
