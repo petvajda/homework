ac5611d63da9b4a3c7c25047aa69393b20c571e0
Peter Vajda <petvajda@gmail.com>
2015-04-21 11:52:22 +0200

    Change the order of assertions

--- test.rb
+++ test.rb
@@ -6,10 +6,10 @@ class TestKata04 < Test::Unit::TestCase
   def test_weather
   	d = WeatherData.new
     d.read_file("weather.dat")
-    assert_equal([14, 2], d.analyse())
     assert_equal(30, d.temperature_spread.length)
+    assert_equal([14, 2], d.analyse())
   end
-  
+
   def test_football
   	d = FootballData.new
     d.read_file("football.dat")
