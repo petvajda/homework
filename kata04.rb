fb022e0b0b368fd76a9442890feec6dea8d32eb7
Peter Vajda <petvajda@gmail.com>
2015-04-22 10:32:54 +0200

    Remove the ugly big number

--- kata04.rb
+++ kata04.rb
@@ -5,22 +5,26 @@
 
 require 'csv'
 require_relative "kata04"
-FIXNUM_MAX = (2**(0.size * 8 -2) -1)
 
 class DataReader
   attr_accessor :data
   
   def read_file(file_name, name_i, first_i, second_i)
-    @data = Array.new
-    smallest = ["", FIXNUM_MAX]
+    @data    = Array.new
+    firstrun = true
+    smallest = []
     CSV.foreach(file_name, col_sep: " ") do |row|
       begin
-        name          = row[name_i]
-        data1         = Integer(row[first_i].chomp("*"))
-        data2         = Integer(row[second_i].chomp("*"))
-        diff          = (data2 - data1).abs
-        smallest      = name, diff if diff < smallest[1]
-        @data        << [name, data1, data2]
+        name                 = row[name_i]
+        data1                = Integer(row[first_i].chomp("*"))
+        data2                = Integer(row[second_i].chomp("*"))
+        diff                 = (data2 - data1).abs
+        if firstrun
+          smallest, firstrun = [row[name_i], diff], false
+        else
+          smallest           = name, diff if diff < smallest[1]
+        end        
+        @data               << [name, data1, data2]
       rescue ArgumentError, NoMethodError
         # Skip header of table and empty lines
       end
